81ff7440834a70443b37d1f5c12d802ae65be1e2
Hugglermatheus125 <matheushuggler125@gmail.com>
2025-03-13 18:28:43 -0300

    Aula 13/03

---
+++ 05_include_require/china.php
@@ -0,0 +1,25 @@
+<?php include './header.php';?>
+
+
+<div.container-fluid>
+    <div class="row">
+        <div class="col-12" d-flex justify-content-center align-itens-center>
+            <h1>Muralha da China</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <img style='width 100%;' src="https://delfos.net.br/wp-content/uploads/2017/04/813388-25-04-17-06.jpg">
+            </div> 
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img style='width 100%;' src="https://delfos.net.br/wp-content/uploads/2017/04/813388-25-04-17-06.jpg">
+            </div> 
+        </div>
+    </div>
+</div>
+
+
+<?php include './footer.php';?>
